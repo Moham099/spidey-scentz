056ba4cc8e7fcf6c8c90688722f965f72b2323fb
Mo Nasser <mahmood.mohammednasser@gmail.com>
2022-04-18 16:35:06 +0900

    announcement banner complete

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Announcement from '../components/Announcement'
+
+
+export const Landing = () => {
+  return (
+    <div>
+        <Announcement/>
+    </div>
+  )
+}
+
+export default Landing
